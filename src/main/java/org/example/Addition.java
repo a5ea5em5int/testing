cc4c4061f75c3bccc42a00fcfe58f14f609b4d0b
40012435 <a.myint@napier.ac.uk>
2022-03-25 20:26:06 +0630

    commit master

---
+++ src/main/java/org/example/Addition.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Addition {
+    public String getA() {
+        return a;
+    }
+
+    public Addition(String a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    String a;
+    String b;
+
+}
